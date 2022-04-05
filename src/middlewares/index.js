9635e2ac1e636a53d59a6be0020ee189c234fe4e
Nabeel Sajid <nabeel.sajid@qbatch.com>
2022-04-06 01:46:26 +0500

    removed timeout

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -3,12 +3,10 @@ import cors from "cors";
 import passport from "passport";
 import logger from "morgan";
 import bodyParser from "body-parser";
-import timeout from "connect-timeout";
 
 import { LocalLoginStrategy, AuthenticationStrategy } from "./auth";
 
 const applyMiddlewares = (app) => {
-  app.use(timeout('500000s'));
   app.use(cors());
   app.use(bodyParser.json({ limit: "50mb" }));
   app.use(express.json());
