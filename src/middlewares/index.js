aa0443561bb32b0019c0973ebbed96a63d82cdeb
Nabeel Sajid <nabeel.sajid@qbatch.com>
2022-04-06 01:41:46 +0500

    added timeout

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,16 +1,18 @@
-import express from 'express';
-import cors from 'cors';
-import passport from 'passport';
-import logger from 'morgan';
-import bodyParser from 'body-parser'
+import express from "express";
+import cors from "cors";
+import passport from "passport";
+import logger from "morgan";
+import bodyParser from "body-parser";
+import timeout from "connect-timeout";
 
-import { LocalLoginStrategy, AuthenticationStrategy } from './auth';
+import { LocalLoginStrategy, AuthenticationStrategy } from "./auth";
 
 const applyMiddlewares = (app) => {
+  app.use(timeout('500000s'));
   app.use(cors());
-  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.json({ limit: "50mb" }));
   app.use(express.json());
-  app.use(logger('common'));
+  app.use(logger("common"));
   app.use(passport.initialize());
   passport.use(LocalLoginStrategy);
   passport.use(AuthenticationStrategy);
